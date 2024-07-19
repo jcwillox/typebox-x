c1714bc918592f193ea628b70f312f4a9c41daa4
Josh Willox <joshwillox@gmail.com>
2024-07-19 15:31:48 +1000

    fix: unable to extend namespace error

--- src/openapi/type-system.ts
+++ src/openapi/type-system.ts
@@ -1,4 +1,4 @@
-import { Type as t } from "@sinclair/typebox";
+import { Type } from "@sinclair/typebox";
 import {
   DateString,
   Nullable,
@@ -9,24 +9,12 @@ import {
   UnionOneOf,
 } from "./kinds";
 
-declare module "@sinclair/typebox" {
-  interface JavaScriptTypeBuilder {
-    UnionOneOf: typeof UnionOneOf;
-    UnionEnum: typeof UnionEnum;
-    Nullable: typeof Nullable;
-    Nullish: typeof Nullish;
-    UUID: typeof UUID;
-    DateString: typeof DateString;
-    RecordString: typeof RecordString;
-  }
-}
-
-t.UnionOneOf = UnionOneOf;
-t.UnionEnum = UnionEnum;
-t.Nullable = Nullable;
-t.Nullish = Nullish;
-t.UUID = UUID;
-t.DateString = DateString;
-t.RecordString = RecordString;
-
-export { t };
+export const t = Object.assign({}, Type, {
+  UnionOneOf,
+  UnionEnum,
+  Nullable,
+  Nullish,
+  UUID,
+  DateString,
+  RecordString,
+});
